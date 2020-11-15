4f237b22ccc669aaae701a205351524ea8d74c10
Konstantin-Bruno <Konstantin-Bruno@users.noreply.github.com>
2020-11-15 21:50:38 +0200

    Update task-5.js

--- task-5.js
+++ task-5.js
@@ -1,10 +1,12 @@
 const input = document.querySelector('#name-input');
 const name = document.querySelector('#name-output');
-function textInput() {    
- if(input.value){
-name.textContent = input.value;
-}
-name.textContent = "незнакомец";
+function textInput() {
+    if (input.value) {
+        name.textContent = input.value;
+    }
+    else {
+        name.textContent = "незнакомец";
+    }
 }
 
 input.addEventListener('keyup', textInput);
