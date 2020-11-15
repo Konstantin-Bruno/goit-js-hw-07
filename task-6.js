d3ca048a99c40ebcb5a8ebab85131967ceb94ed1
Konstantin-Bruno <Konstantin-Bruno@users.noreply.github.com>
2020-11-15 21:25:24 +0200

    Update task-6.js

--- task-6.js
+++ task-6.js
@@ -1,15 +1,11 @@
 function textInput() {
   const length = input.getAttribute('data-length');
-  classListAdd(input.value.length === Number(length) ? true : false);
-}
-function classListAdd(added) {
-  if (added) {
-    input.classList.add('valid');
+if(input.value.length === Number(length)){
+input.classList.add('valid');
     input.classList.remove('invalid');
-  } 
-    input.classList.add('invalid');
+} else {input.classList.add('invalid');
     input.classList.remove('valid');
-  
+}
 }
 const input = document.querySelector('#validation-input');
 
