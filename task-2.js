5bbe3d675d945c4a12a73d2a042575f41f433a6f
Konstantin-Bruno <Konstantin-Bruno@users.noreply.github.com>
2020-11-17 10:32:19 +0200

    fixed task-2

--- task-2.js
+++ task-2.js
@@ -1,3 +1,5 @@
+
+const ingredientRef = document.querySelector('#ingredients');
 const ingredients = [
   'Картошка',
   'Грибы',
@@ -6,48 +8,19 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
-const ingredientRef = document.querySelector('#ingredients');
-// ingredients.forEach(ingredient => console.log(ingredient));
-const ingredient = ingredients.map(ingredient => ingredient);
-console.log(ingredients);
+
+const ingredientRefChild = document.createElement('li');
+let ingredient = ingredients.map(elem => {
+  const li = ingredientRefChild.cloneNode(true);
+  li.textContent = elem;
+  return li;
+});
+ingredientRef.append(...ingredient);
 
 
-console.log(ingredientRef);
 
-const ingredientRefChild1 = document.createElement('li');
-// console.log(ingredientRefChild1);
-ingredientRef.appendChild(ingredientRefChild1);
-ingredientRefChild1.textContent = ingredient[0];
-// console.log(ingredientRef);
 
-const ingredientRefChild2 = document.createElement('li');
-// console.log(ingredientRefChild2);
-ingredientRef.appendChild(ingredientRefChild2);
-ingredientRefChild2.textContent = ingredient[1];
-// console.log(ingredientRef);
 
-const ingredientRefChild3 = document.createElement('li');
-// console.log(ingredientRefChild3);
-ingredientRef.appendChild(ingredientRefChild3);
-ingredientRefChild3.textContent = ingredient[2];
-// console.log(ingredientRef);
 
-const ingredientRefChild4 = document.createElement('li');
-// console.log(ingredientRefChild4);
-ingredientRef.appendChild(ingredientRefChild4);
-ingredientRefChild4.textContent = ingredient[3];
-// console.log(ingredientRef);
 
-const ingredientRefChild5 = document.createElement('li');
-// console.log(ingredientRefChild5);
-ingredientRef.appendChild(ingredientRefChild5);
-ingredientRefChild5.textContent = ingredient[4];
-// console.log(ingredientRef);
-
-const ingredientRefChild6 = document.createElement('li');
-// console.log(ingredientRefChild6);
-ingredientRef.appendChild(ingredientRefChild6);
-ingredientRefChild6.textContent = ingredient[5];
-
-console.log(ingredientRef);
 
